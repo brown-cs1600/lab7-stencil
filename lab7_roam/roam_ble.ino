db84a0e557217d195c2bedb0c9fab9bfb45c0c03
mziz <milda_zizyte@brown.edu>
2024-10-29 15:28:09 -0400

    Update roam_ble.ino

--- lab7_roam/roam_ble.ino
+++ lab7_roam/roam_ble.ino
@@ -6,8 +6,15 @@ static String savedAddress = "";
  */
 void roamInit() {
   if (!BLE.begin()) {
-    Serial.println("starting Bluetooth® Low Energy module failed!");
-    while (1);
+    Serial.println("BLE failed to start. Attempting a reset...");
+    Serial.println("Please be patient while you wait for the device to reconnect");
+    Serial.println("Don't unplug or reset your device! Just close and open the serial monitor again if nothing appears,");
+    Serial.println("and send any character to begin the sketch");
+    delay(1000);
+    static const char RESET[] = "AT+RESET\n";
+    Serial2.write(RESET, sizeof(RESET) - 1);
+    delay(2000);
+    NVIC_SystemReset();
   }
 
   // Scan for corresponding home device
