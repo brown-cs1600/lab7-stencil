b9f0a4612361bc35c362c526d43b0cb9387d3294
mziz <milda_zizyte@brown.edu>
2024-10-29 15:24:08 -0400

    Update lab7_roam.ino

--- lab7_roam/lab7_roam.ino
+++ lab7_roam/lab7_roam.ino
@@ -3,6 +3,9 @@
 void setup() {
   Serial.begin(9600);
   while(!Serial);
+  Serial.println("Send any character to begin");
+  while (!Serial.available()) { delay(100); }
+  Serial.println("Roam code started!");
 
   // Compute monster name and key and send to home device for initialization
   roamInit();
